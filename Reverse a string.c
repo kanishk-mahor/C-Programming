2f3a6af79103e21d108cc59fca1e6f1f5394e477
kanishk-mahor <mahorkanishk@gmail.com>
2020-02-02 12:57:50 +0530

    c program

--- Reverse a string.c
+++ Reverse a string.c
@@ -5,12 +5,11 @@ int i,length_of_array;
 
 int main()
 {
-    printf("Enter maximum no of character of string\n");///////size of character array////////
+    printf("enter a string to reverse\n");              ///////your string u want to reverse without space/////
     scanf("%d",&length_of_array);
     char str[length_of_array];
-    printf("enter a string to reverse\n");              ///////your string u want to reverse without space/////
 
-    scanf("%s",str);
+    scanf("%[^\n]",str);
     printf("size of string is entered: %d\n",strlen(str));
 
     for(i=1;i<=strlen(str);++i)
